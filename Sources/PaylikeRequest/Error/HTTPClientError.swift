843742fa3bab822c347d399c5ed52ed484fc5b85
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-05-18 11:54:50 +0200

    feat(error): added conformation to LocalizedError protocol to the HTTPClientError

--- Sources/PaylikeRequest/Error/HTTPClientError.swift
+++ Sources/PaylikeRequest/Error/HTTPClientError.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 /**
- Describes errors regarding the library
+ * Describes errors regarding the HTTP Client and it's close components
  */
-public enum HTTPClientError : Error {
+public enum HTTPClientError : Error, LocalizedError {
     /**
      * Unknown error happened during the request and the response cannot be created
      */
@@ -28,4 +28,24 @@ public enum HTTPClientError : Error {
      Happens when the response body is empty and / or cannot be represented as a String
      */
     case ResponseCannotBeSerializedToString(_ response: URLResponse)
+    
+    /**
+     * Localized text of the error messages
+     */
+    public var errorDescription: String? {
+        switch self {
+            case .UnknownError:
+                return "UnknownError"
+            case .InvalidURL(url: let url):
+                return "Invalid URL: \(url.absoluteString)"
+            case .NotHTTPURLResponse(_):
+                return "Not HTTP URL response"
+            case .NoHTTPResponse(_, _):
+                return "Not HTTP Resposne"
+            case .ResponseCannotBeSerializedToJSON(_):
+                return "Response cannot be serialized to JSON"
+            case .ResponseCannotBeSerializedToString(_):
+                return "Response cannto be serialized to String"
+        }
+    }
 }
